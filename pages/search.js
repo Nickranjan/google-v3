fc6edfd209f47daa93150a0daa38d9b0910e360e
Rhishish Ranjan <rhishishranjan1@gmail.com>
2022-07-24 00:24:50 +0530

    Fix the remaining buttons plus I'm feeling lucky as well as the secret

--- pages/search.js
+++ pages/search.js
@@ -34,7 +34,7 @@ export async function getServerSideProps(context){
     const data = mockData ? Response 
     : await fetch(
         
-       `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_key}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}` 
+       `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}` 
     ) .then((response)=>response.json())
     return{
         props:{
