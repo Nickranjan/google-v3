acae70ecab598ff88da1d6c6a1e4c0013984b348
Rhishish Ranjan <rhishishranjan1@gmail.com>
2022-07-26 11:32:37 +0530

    fixing eror

--- components/searchHeaderOption.js
+++ components/searchHeaderOption.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 
 export default function SearchHeaderOption({ title, Icon, selected }) {
-  const router = useRouter();
+  const router = useRouter(); //ushing router 
   function selectTab(title) {
     router.push(
       `/search?term=${router.query.term}&searchType=${
@@ -11,13 +11,13 @@ export default function SearchHeaderOption({ title, Icon, selected }) {
   }
   return (
     <div
-      onClick={()=>selectTab(title)}
+      onClick={()=>selectTab(title)} //adding onclick tp click easily 
       className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${
-        selected && "text-blue-500 border-blue-500"
+        selected && "text-blue-500 border-blue-500" //blue botder .
       }`}
     >
       <Icon className="h-4" />
       <p>{title}</p>
     </div>
-  );
+  )
 }
